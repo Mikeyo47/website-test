07bea35f877baaecd0eacacf118a6e15e9c7b9a4
Mikeyo47 <kopecky_michal@yahoo.com>
2023-05-10 00:00:17 +0100

    Started adding Blackjack

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+
+//Simple Calculator
 function add() {
     const numA = Number(document.getElementById("num1-el").value);
     const numB = Number(document.getElementById("num2-el").value);
@@ -23,3 +25,26 @@ function multiply() {
 }
 
 
+//Blackjack
+
+function deal() {
+let firstCard = Math.floor(Math.random() * 10) + 2;
+let secondCard = Math.floor(Math.random() * 10) + 2;
+let sum = firstCard + secondCard;
+document.getElementById("cards-el").textContent += `${firstCard}, ${secondCard}`;
+document.getElementById("cards-sum-el").textContent += sum;
+}
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+
+if (sum < 21) {
+    message = "Do you want to draw a new card? 🙂";
+} else if (sum === 21) {
+    hasBlackJack = true;
+    message = "Wohoo! You've got Blackjack! 🥳";
+} else {
+    isAlive = false;
+    message = "You're out of the game! 😭";
+}
